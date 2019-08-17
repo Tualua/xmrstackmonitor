98f6de30bbbe760cd24990f79a9ef34288cad508
Tualua <dsp@mvdnet.org>
2019-08-17 22:52:18 +0800

    1

---
+++ XMRPoolMonitor.psm1
@@ -0,0 +1,22 @@
+﻿function Get-MinerPoolStats
+{
+	param (
+		[Parameter(Mandatory = $true)]
+		[string]$Worker,
+		[Parameter(Mandatory = $true)]
+		[string]$Wallet
+	)
+	$ResponseData = ''
+	$PoolAPIUrl = "https://www.supportxmr.com/api/miner/$Wallet/stats/$Worker"
+	Try
+	{
+		$ResponseData = Invoke-RESTMethod -Uri $PoolAPIUrl -TimeoutSec 10
+	}
+	Catch [Net.WebException]
+	{
+		Write-Verbose "An exception was caught connecting to pool : $($_.Exception.Message)"
+	}
+	
+	return $ResponseData.hash
+	
+}
